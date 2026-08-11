4f2c76dfb465f7f64a92af6584b5e2e9461e1f9f
librehyangSu <librehyangsu@gmail.com>
2026-08-11 10:11:59 +0900

    style(core): CachedCircleAvatar에 backgroundColor 옵션 추가

--- lib/core/widgets/cached_circle_avatar.dart
+++ lib/core/widgets/cached_circle_avatar.dart
@@ -4,16 +4,27 @@ import 'package:flutter/material.dart';
 class CachedCircleAvatar extends StatelessWidget {
   final String? url;
   final double? radius;
+  final Color? backgroundColor;
   final Widget? child;
 
-  const CachedCircleAvatar({super.key, this.url, this.radius, this.child});
+  const CachedCircleAvatar({
+    super.key,
+    this.url,
+    this.radius,
+    this.backgroundColor,
+    this.child,
+  });
 
   @override
   Widget build(BuildContext context) {
     return CircleAvatar(
       radius: radius,
       backgroundImage: (url == null) ? null : CachedNetworkImageProvider(url!),
-      onBackgroundImageError: (_, __) {},
+      backgroundColor: backgroundColor,
+      onBackgroundImageError: (e, st) {
+        // print("Avatar Error: ${url!}");
+        // print(e);
+      },
       child: child,
     );
   }
