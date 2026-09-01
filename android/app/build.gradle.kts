d32c1b47b523c70946ede3980ecc5142d4985ae0
librehyangSu <librehyangsu@gmail.com>
2026-09-01 16:41:17 +0900

    fix(android): compileSdk 37로 상향 (flutter_secure_storage, permission_handler 요구)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.danmalgi.mobile"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
